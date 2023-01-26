042c1878d4bb348fcf2c7a2651b68f9a5064daf8
WanyonyiKenya <brianwanyonyike@gmail.com>
2023-01-26 17:40:41 +0300

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+const sideMenu = document.querySelector("aside")
+const menuBtn = document.querySelector("#menu-btn")
+const closeBtn = document.querySelector("#close-btn")
+const themeToggler = document.querySelector(".theme-toggler")
 
+menuBtn.addEventListener('click', ()=>{
+  sideMenu.style.display = 'block'
+})
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+closeBtn.addEventListener('click', ()=>{
+  sideMenu.style.display = 'none'
+})
 
+themeToggler.addEventListener('click', ()=>{
+  document.body.classList.toggle('dark-theme-variables');
+
+  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+})
+
+Orders.forEach(order => {
+  const tr = document.createElement('tr');
+  const trContent = `
+              <td>${order.productName}</td>    
+              <td>${order.productNumber}</td>   
+              <td>${order.paymetStatus}</td>   
+              <td class ="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'Pending' ? 'warning' : 'primary'} ">${order.shipping}</td>   
+              <td>Details</td>     
+
+    `;
+
+    tr.innerHTML =trContent;
+    document.querySelector('table tbody').appendChild(tr)
+})
---
+++ orders.js
@@ -0,0 +1,62 @@
+const Orders = [
+    {
+        productName:'2kgs Sugar',
+        productNumber: '26574',
+        paymetStatus: 'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName:'2kgs salt',
+        productNumber: '26579',
+        paymetStatus: 'Refunded',
+        shipping : 'Declined'
+    },
+    {
+        productName:'Bar Soaps',
+        productNumber: '116574',
+        paymetStatus: 'Due',
+        shipping : 'Delivered'
+    },
+    {
+        productName:'Wheat flour',
+        productNumber: '97574',
+        paymetStatus: 'Paid',
+        shipping : 'Pending'
+    },
+    {
+        productName:'Baking Powder',
+        productNumber: '007574',
+        paymetStatus: 'Processing',
+        shipping : 'Delivered'
+    },
+    {
+        productName:'Washing Soap',
+        productNumber: '54374',
+        paymetStatus: 'Refunded',
+        shipping : 'Declined'
+    },
+    {
+        productName:'Cooking Oil',
+        productNumber: '999574',
+        paymetStatus: 'Paid',
+        shipping : 'Pending'
+    },
+    {
+        productName:'KitchenWare',
+        productNumber: '35674',
+        paymetStatus: 'Paid',
+        shipping : 'Delivered'
+    },
+    {
+        productName:'Cooking Gas',
+        productNumber: '0000',
+        paymetStatus: 'Refunded',
+        shipping : 'Declined'
+    },
+    {
+        productName:'Table Salt',
+        productNumber: '11574',
+        paymetStatus: 'Due',
+        shipping : 'Pending'
+    }
+]
